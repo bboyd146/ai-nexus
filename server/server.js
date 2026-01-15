4283ac32211f9aec4ca470d59e7e837775bc79a4
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2026-01-14 19:18:43 -0600

    changed server to include correct routes

--- server/server.js
+++ server/server.js
@@ -1,23 +1,21 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import chatRoutes from "./routes/api/chat.js";
+import router from "./routes/index.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config();
-
 const app = express();
+const PORT = process.env.PORT || 5004;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api/chat", chatRoutes);
+app.use("/api", router);
 
 // Serve frontend build
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
@@ -33,5 +31,4 @@ app.use((err, req, res, next) => {
 });
 
 
-const PORT = process.env.PORT || 5004;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
