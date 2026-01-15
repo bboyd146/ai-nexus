08fafbcddd696dbaa2444b1727d2dec9e3a4943f
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2026-01-14 19:05:49 -0600

    added necessary imports

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import chatRoutes from "./routes/api/chat.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
