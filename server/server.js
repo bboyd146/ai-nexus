b856c5827f958783ebe8136ff61cb41955f53654
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2026-01-14 21:24:24 -0600

    changed wildcard route to /

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use("/api", router);
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // React catch-all (MUST be last)
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
